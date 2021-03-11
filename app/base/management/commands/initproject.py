ad28d6e1472fe885879d9e3a879082ec8fd2bf66
Fasih Ahmad Fakhri <fasih@email.com>
2021-03-11 16:10:13 +0530

    loaded fixtures at project initialization

--- app/base/management/commands/initproject.py
+++ app/base/management/commands/initproject.py
@@ -1,3 +1,4 @@
+from django.core import management
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 
@@ -9,15 +10,17 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         User = get_user_model()
         if User.objects.count() == 0:
-            #management.call_command('loaddata', 'accounts', verbosity=1)
-            #management.call_command('loaddata', 'billing', verbosity=1)
-            admin = User.objects.create_superuser(
-                        username='admin', email="admin@mayafin.in",
+            admin = User.objects.create_superuser(username='admin',
+                        email="admin@mayafin.in",
                         password='google@123')
             admin.is_active = True
             admin.is_admin = True
             admin.is_staff = True
             admin.save()
+
+            management.call_command('loaddata', 'platforms', verbosity=1)
+            management.call_command('loaddata', 'borrowers', verbosity=1)
+            management.call_command('loaddata', 'lenders', verbosity=1)
 
             self.stdout.write(self.style.SUCCESS('Created superuser and loaded module fixtures.'))
             self.stdout.write(self.style.SUCCESS('Project initialization completed :)'))
